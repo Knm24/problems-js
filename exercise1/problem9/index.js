ca38b99d299be92a4ee3ea2c778fe5aa2ef409d8
Knm24 <sun.forsurface@gmail.com>
2023-08-31 01:47:49 +0600

    add: code ex9 num%10

--- exercise1/problem9/index.js
+++ exercise1/problem9/index.js
@@ -1,5 +1,30 @@
 function isDisarium() {
-  // Your code
+
+  let newArr = [];
+  let num = arguments[0]; 
+  let kaldyk; 
+  let counter = 0;
+
+  while (num > 0) 
+  {   
+     kaldyk = (num%10);
+     num = Math.trunc(num/10);
+     newArr.push(kaldyk);
+    counter++;
+
+  }
+newArr.reverse();
+newArr1 = newArr.map((n, index) => Math.pow(n, index+1));
+let summator = 0;
+newArr2 = newArr1.map((n, index) => summator =  summator + n);
+
+if(summator == arguments[0])
+return true;
+else
+return false;
+
+
+
 }
 
 module.exports = isDisarium;
