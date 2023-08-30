245d59d73996e445637c6a024634a5f680c14b61
Knm24 <sun.forsurface@gmail.com>
2023-08-30 23:28:32 +0600

    add code ex6

--- exercise1/problem6/index.js
+++ exercise1/problem6/index.js
@@ -1,5 +1,12 @@
 function insertWhitespace() {
-  // Your code
+  let strArr = arguments[0].split(""); 
+  const newStr = strArr.map((s) => ( (s == s.toUpperCase()) ? ' ' + s : '' + s ) ).join('');
+
+ 
+  if(arguments[0] ==  newStr)
+    return null
+  else
+    return newStr;
 }
 
 module.exports = insertWhitespace;
