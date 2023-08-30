d9c8e0cd5a37d34981d315ea635b39f938a4a025
Knm24 <sun.forsurface@gmail.com>
2023-08-30 23:42:30 +0600

    add: code ex10

--- exercise1/problem10/index.js
+++ exercise1/problem10/index.js
@@ -1,5 +1,9 @@
 function move() {
-  // Your code
+  let strArr = arguments[0].split(""); 
+  const newStr = strArr.map((s) => ( (s == s) ?  String.fromCharCode(Number(s.charCodeAt())  + 1) : '' + s ) ).join('');
+
+
+  return newStr.trim();
 }
 
 module.exports = move;
