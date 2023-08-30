a563c37c848d51b80662cfcb135062e82621324c
Knm24 <sun.forsurface@gmail.com>
2023-08-30 22:51:50 +0600

    add code ex4

--- exercise1/problem4/index.js
+++ exercise1/problem4/index.js
@@ -1,5 +1,8 @@
 function makeTitleCase() {
-  // Your code
+  let strArr = arguments[0].split(" "); 
+
+  const newStr = strArr.map((s) => ( (s.length == 1) ? s[0].toUpperCase() : s[0].toUpperCase() + s.slice(1 - s.length)) );
+  return newStr.join(' ');
 }
 
 module.exports = makeTitleCase;
