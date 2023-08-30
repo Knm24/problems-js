8874336400e3f3d6bb5188405685fef2e5087616
Knm24 <sun.forsurface@gmail.com>
2023-08-31 00:41:31 +0600

    add: code ex3 penta

--- exercise1/problem3/index.js
+++ exercise1/problem3/index.js
@@ -1,5 +1,21 @@
 function pentagonal() {
-  // Your code
+
+  let num = arguments[0]; 
+
+let outside = 5;
+let inner = 1;
+let penta;
+
+if(num == 1)
+return 1;
+
+for (i = 1; i <= num; i++) 
+{   
+  penta =  (i*(outside*i-outside) /2)+inner;   
+}
+
+return penta;
+
 }
 
 module.exports = pentagonal;
