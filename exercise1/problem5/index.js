38c95a9776745f794107d57be0f73ae011bfd833
Knm24 <sun.forsurface@gmail.com>
2023-08-30 23:21:46 +0600

    add code ex5

--- exercise1/problem5/index.js
+++ exercise1/problem5/index.js
@@ -1,5 +1,12 @@
 function removeABC() {
-  // Your code
+  let strArr = arguments[0].split(""); 
+  const newStr = strArr.map((s) => ( (s == 'a' ||  s == 'b' ||  s == 'c') ? '' : '' + s ) ).join('');
+
+ 
+  if(arguments[0] ==  newStr)
+    return null
+  else
+    return newStr;
 }
 
 module.exports = removeABC;
