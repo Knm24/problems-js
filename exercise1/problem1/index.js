9dc1f561f09061c9673a75d5aa9439b93ef69159
Knm24 <sun.forsurface@gmail.com>
2023-08-30 22:35:46 +0600

    add code ex2

--- exercise1/problem1/index.js
+++ exercise1/problem1/index.js
@@ -1,6 +1,17 @@
 function numOfDigits(int) {
   // Your code
-  console.log('int:' + int);
+
+ let counter = 0;
+ if(int < 0) int = Math.abs(int);
+ if(int == 0) return 1;
+
+ while (int > 0) 
+ {
+    int = Math.trunc(int / 10);
+    counter = counter + 1;
+ }
+
+  return counter;
 }
 
 module.exports = numOfDigits;
--- exercise1/problem2/index.js
+++ exercise1/problem2/index.js
@@ -1,5 +1,16 @@
 function intWithinBounds() {
-  // Your code
+    n = arguments[0];
+    low = arguments[1];
+    up = arguments[2];    
+    
+    if( n % 1 != 0) {
+      return false;
+    }
+    
+    if(n > low && n  < up )    
+      return true; 
+
+      return false;
 }
 
 module.exports = intWithinBounds;
