7965b2070ddc93d2db16a9318a6d1b58e0ba0bc3
Knm24 <sun.forsurface@gmail.com>
2023-09-01 00:31:13 +0600

    add: code ex8 \sec

--- exercise1/problem8/index.js
+++ exercise1/problem8/index.js
@@ -1,5 +1,15 @@
-function longestTime() {
-  // Your code
+function longestTime() 
+{
+  
+
+  let hours = arguments[0]/3600;
+  let min = arguments[1]/60;
+  let sec = arguments[2];
+
+  return Math.max(hours, min, sec);
+
 }
 
-module.exports = longestTime;
+
+
+// module.exports = longestTime;
