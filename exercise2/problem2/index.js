00c5058cc0b1975028a6a0db9897d3dfb1e4bd34
Knm24 <sun.forsurface@gmail.com>
2023-09-06 22:17:11 +0600

    add ex2p2

--- exercise2/problem2/index.js
+++ exercise2/problem2/index.js
@@ -1,5 +1,34 @@
 function sortIt() {
-  // Your code
-}
 
-module.exports = sortIt;
+  let newArr = arguments[0];
+  let tempPivot = [newArr[0]];
+  let leftArr = [];
+  let rightArr = [];
+  
+  if (newArr.length < 2) return newArr;
+  
+      for (let index = 1; index < newArr.length; index++) {
+        let element;
+        if(typeof newArr[index] == Array )
+          element = newArr[index][0];
+        else
+          element = newArr[index];
+
+          //console.log('element:' + element);
+          if (tempPivot > element) {
+  
+              leftArr.push(element);
+  
+          } else {
+              rightArr.push(element);
+  
+          }
+  
+      }
+      return [...sortIt(leftArr), ...tempPivot, ...sortIt(rightArr)]
+    }
+
+
+
+ module.exports = sortIt;
+
