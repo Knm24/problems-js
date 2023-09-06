cea53175b7339cf97969c62416fc3dcd84eeeb75
Knm24 <sun.forsurface@gmail.com>
2023-09-06 21:49:12 +0600

    add ex2p1

--- exercise2/problem1/index.js
+++ exercise2/problem1/index.js
@@ -1,5 +1,12 @@
-function arrayOfMultiples() {
-  // Your code
-}
+function arrayOfMultiples(num, len) {
+  let newArr = [];
+  let counter = 0;
+  while (len > 0) {   
+    let bb= num *( counter+1);
+    newArr.push(bb);
+    counter++;
+    len--;
+  }
+return newArr;}
 
 module.exports = arrayOfMultiples;
