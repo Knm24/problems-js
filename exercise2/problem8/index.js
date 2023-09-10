a6f251f72ae932fb31b7974edddf025e346d9375
Knm24 <sun.forsurface@gmail.com>
2023-09-11 03:41:38 +0600

    add ex2p 8

--- exercise2/problem8/index.js
+++ exercise2/problem8/index.js
@@ -1,20 +1,82 @@
 function isEqual(oneObj, twoObj) {
+  
 
-  let isEqual = false;
+  let isEq = false;
+  
+  if (Object.keys(oneObj).length === 0 && Object.keys(twoObj).length === 0) 
+  return true;
 
+
+  
+  if (typeof oneObj === 'string' && typeof twoObj === 'string') {
+      
+
+      if(oneObj === twoObj)
+      eq = true;
+      else
+      eq = false;
+  }
+
+  if (typeof oneObj === 'number' && typeof twoObj === 'number') {
+      
+
+      if(oneObj === twoObj)
+      eq = true;
+      else
+      eq = false;
+  }
+
+
+  if (Array.isArray(oneObj) && Array.isArray(twoObj))
+  {
+
+      if(JSON.stringify(oneObj) === JSON.stringify(twoObj)
+      )
+      {
+
+          return true;
+      }
+      else
+      {
+
+          return false;
+      }
+
+  }
 
   for (const key in oneObj) {
-  ww = 'tt'
-  len = twoObj[key].length;
 
     const element = oneObj[key];
+    const element2 = twoObj[key];
 
-    if(twoObj[key] == element)
-    isEqual = true;
+  
+
+
+
+
+    if (typeof oneObj[key] == "object" && typeof twoObj[key] == "object")
+    {
+      return isEqual(oneObj[key], twoObj[key]);
+
+    }
     else
-    isEqual = false;
+    {
+      
+    if(element === element2)
+    {
+      isEq = true;
+
+    }
+    else
+    {
+      isEq = false;
+      return false;
+    }
+    }
+ 
 }
- return isEqual;
+ return isEq;
 }
+
 
 module.exports = isEqual;
