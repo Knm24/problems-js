36fbe271184caef38c2364e0a70f8fab1766c178
Knm24 <sun.forsurface@gmail.com>
2023-10-19 23:45:23 +0600

    add ex4 pr2 done

--- exercise4/problem2/index.ts
+++ exercise4/problem2/index.ts
@@ -1,3 +1,18 @@
-class Name {}
+class Name {
+    firstName: string; 
+    lastName: string;
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName[0].toUpperCase() + firstName.substring(1).toLowerCase();
+        this.lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1).toLowerCase();
+    }
+
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    get initials(): string {
+        return `${this.firstName[0]}.${this.lastName[0]}`;
+    }
+}
 
 export default Name;
