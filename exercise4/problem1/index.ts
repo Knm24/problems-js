43ae3f18a96d8ddf89d9ffc96cba52ae5d9ea6e2
Knm24 <sun.forsurface@gmail.com>
2023-10-19 22:46:21 +0600

    add ex4 ex1 done

--- exercise4/problem1/index.ts
+++ exercise4/problem1/index.ts
@@ -1,7 +1,33 @@
+
+interface Person {
+    name: string;
+    age: number;    
+    compareAge: Function;
+}
+
 class Person {
-    constructor(){
-        
+
+    constructor(name : string, age: number){
+        this.name = name;
+        this.age = age;
+        this.compareAge = function (instPerson: Person): string {
+
+            let str:string = instPerson.name;
+
+            if(this.age < instPerson.age) 
+                str += ` is older than me.`;            
+            else if(this.age == instPerson.age)  
+                str += ` is the same age as me.`;            
+            else 
+                str += ` is younger than me.`;       
+            
+            return str;
+        };
     }
+
+    
+
+
 }
 
 export default Person;
