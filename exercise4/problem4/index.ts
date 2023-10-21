fe19b40fd0e844310ae2ee52a86e86912097167b
Knm24 <sun.forsurface@gmail.com>
2023-10-22 02:26:01 +0600

    add ex4 pr4 done interface Class=> implements Figure interface

--- exercise4/problem4/index.ts
+++ exercise4/problem4/index.ts
@@ -1,8 +1,45 @@
 // Update it as much as you want, just don't change the name
-export class Circle {}
+
+
+
+interface Figure {
+    calcArea(): void     
+}
+
+
+ export class Circle implements Figure{
+    radius: number;
+    constructor(radius: number){
+        this.radius = radius;
+        
+    }
+    calcArea(): number {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+}
 
 // Update it as much as you want, just don't change the name
-export class Rectangle {}
+export class Rectangle implements Figure{
+    width: number;
+    height: number;
+
+    constructor(width: number , height: number){
+        this.width = width;
+        this.height = height;
+        
+    }
+
+    calcArea(): number {
+        return this.width * this.height;
+    }
+}
 
 // Update it as much as you want, just don't change the name
-export function sumOfAllAreas() {}
+export function sumOfAllAreas(...arr: (Circle | Rectangle)[]) {
+    let sum:number = 0;
+
+    for (const iterator of arr) {
+        sum += iterator.calcArea();
+    }
+    return sum;
+}
