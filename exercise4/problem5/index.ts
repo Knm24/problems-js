d420007a8c28e2dc388a79418bb0d2b2a47502c8
Knm24 <sun.forsurface@gmail.com>
2023-10-22 03:26:15 +0600

    add ex4 pr5

--- exercise4/problem5/index.ts
+++ exercise4/problem5/index.ts
@@ -1,3 +1,34 @@
-class BankAccount {}
+class BankAccount {
+
+
+    #balance: number = 0;
+
+    constructor() {
+       
+    }
+      withdraw (quantity: number){
+
+        if(quantity <= quantity)
+        {
+            this.#balance = this.#balance - quantity;
+            return this.#balance;
+        }        
+        else
+        return -1;
+      }
+
+      deposit (quantity: number){
+        if(quantity <= 1000000)
+        {
+            this.#balance = this.#balance + quantity;
+            return this.#balance;
+        }        
+        else
+        return -1;
+      }
+
+}
 
 export default BankAccount;
+
+
