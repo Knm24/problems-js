289a2f8edc34c1699f45906bf17e875b80230ae9
Knm24 <sun.forsurface@gmail.com>
2023-10-22 19:03:31 +0600

    add ex4 pr8

--- exercise4/problem8/index.ts
+++ exercise4/problem8/index.ts
@@ -1,3 +1,51 @@
-class Calculator {}
+class Calculator {
+
+    private currValue : number;
+
+    constructor(initValue : number){
+        this.currValue = initValue;
+    }
+
+    add(paramValue: number) : number
+    {
+        return this.currValue = (Number(paramValue) + Number(this.currValue));
+    }
+
+    subtract(paramValue: number) : number
+    {
+        return this.currValue = (Number(this.currValue) - Number(paramValue));
+    }
+
+    multiply(paramValue: number) : number
+    {
+        return this.currValue = (Number(this.currValue) * Number(paramValue));
+    }
+
+    divide(paramValue: number) : unknown
+    {        
+        try {
+            if(paramValue == 0)
+            {
+                throw new Error("err divide");
+            }
+
+            this.currValue = (Number(this.currValue) / Number(paramValue));
+            
+        } catch (e : unknown) {
+
+            console.log(`Error cannot delete (${paramValue})`);
+        }
+
+        return this.currValue;
+    }
+
+    logResult() : void
+    {
+        console.log(Number(this.currValue));
+    }
+
+
+}
 
 export default Calculator;
+
