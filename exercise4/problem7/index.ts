2ab79b4ed959afa3ea318cc8548aa4bdbe613a8b
Knm24 <sun.forsurface@gmail.com>
2023-10-23 22:22:53 +0600

    add ex4 pr7

--- exercise4/problem7/index.ts
+++ exercise4/problem7/index.ts
@@ -1,3 +1,18 @@
-const is;
+const is = Object.assign({});
 
-export default is;
+is.bool = function(val :boolean) {
+    return typeof (val) === 'boolean';
+}
+
+is.num = (val :number) =>  typeof (val) === 'number';
+
+
+is.str = (val :string) => typeof (val) === 'string';
+ 
+
+is.fun = function(val :Function) {
+    return typeof(val) === 'function';
+}
+
+ export default is;
+
