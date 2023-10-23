0befdafa16400df59a1e792600d3b938f5a14e9d
Knm24 <sun.forsurface@gmail.com>
2023-10-23 23:55:43 +0600

    add ex4 pr6 always true

--- exercise4/problem6/index.ts
+++ exercise4/problem6/index.ts
@@ -1,19 +1,83 @@
 // Update it as much as you want, just don't change the names
+// export
+ export class BankAccount {
 
-export class BankAccount {}
 
-export class FedexAccount {}
+  #balance: number = 0;
 
-export class KazPostAccount {}
+  constructor() {
+     
+  }
+  public withdraw (quantity: number){
 
-export function withdrawMoney(accounts, amount) {
+      if(quantity <= quantity)
+      {
+          this.#balance = this.#balance - quantity;
+          return this.#balance;
+      }        
+      else
+      return -1;
+    }
+
+    public deposit (quantity: number){
+      if(quantity <= 1000000)
+      {
+          this.#balance = this.#balance + quantity;
+          return this.#balance;
+      }        
+      else
+      return -1;
+    }
+
+}
+
+
+
+
+
+export class FedexAccount {
+    constructor() {
+      
+    }
+    public sendMail(recipient: string):void  {
+      console.log('greet sendMail ' + recipient );
+    }
+    public recieveMail(sender: string): void {
+      console.log('greet recieveMail probably random text ' + sender);
+
+    }
+
+
+}
+
+
+
+
+export class KazPostAccount extends BankAccount {
+  constructor() {
+     super();
+  }
+
+  public sendMail(recipient: string):void  {
+    console.log('greet sendMail ' + recipient );
+  }
+  public recieveMail(sender: string): void {
+    console.log('greet recieveMail probably random text ' + sender);
+
+  }
+  
+}
+
+export function withdrawMoney(accounts: BankAccount[]  , amount: number) {
   for (const account of accounts) {
     account.withdraw(amount);
   }
 }
 
-export function sendLetterTo(accounts, recipient) {
+export function sendLetterTo(accounts: (FedexAccount | KazPostAccount)[], recipient: string) {
   for (const account of accounts) {
     account.sendMail(recipient);
   }
 }
+
+
