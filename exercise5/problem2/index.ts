15f421bcc348fcc18475a3d2fdbdd445ffaf900e
Knm24 <143102406+Knm24@users.noreply.github.com>
2023-10-31 13:40:12 +0600

    fix: clear ex5 pr2

--- exercise5/problem2/index.ts
+++ exercise5/problem2/index.ts
@@ -1,3 +1,18 @@
-function printNumberInInterval() {}
+
+function log(n) {
+  console.log(n);   
+  if(n == 10) {
+    console.log('fix clear ten');
+
+    clearInterval(timerId);
+    
+  }
+}
+
+function printNumberInInterval(log) {
+  let n = 1;
+  let timerId = setInterval(() => log(n++), 1000);
+}
+
 
 export default printNumberInInterval;
