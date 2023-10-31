a063109d9ccc7b07efa6b966a68f2e330726ed99
Knm24 <143102406+Knm24@users.noreply.github.com>
2023-10-31 13:20:14 +0600

    add code ex5 pr1

--- exercise5/problem1/index.ts
+++ exercise5/problem1/index.ts
@@ -1,3 +1,15 @@
-function callbackExec() {}
+function callbackExec(fnc)
+{
+  setTimeout(fnc, 2000);
+}
 
 export default callbackExec;
+
+/*
+
+function displayHello() {
+    console.log('Hello');
+}
+
+
+*/
