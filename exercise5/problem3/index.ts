0025d87d676ad6d828f5d26ea8b28175a97b4e89
Knm24 <sun.forsurface@gmail.com>
2023-11-01 23:31:01 +0600

    need fix  ex5 pr3

--- exercise5/problem3/index.ts
+++ exercise5/problem3/index.ts
@@ -1,3 +1,31 @@
-function printAsyncNumbers() {}
+function printAsyncNumbers(b: number ) {
 
-export default printAsyncNumbers;
+    async function anyfunc (n:number) : Promise<number> {
+       
+      let timems = n * 10000;
+      //await console.log('timems:' + timems);
+
+
+      await (setTimeout(():void => {}), timems);
+
+      await console.log(n);
+      return n;
+    };
+  
+    let n = 1;
+    
+    let timerId = setInterval(async () => {
+        let timems : number;
+        timems = await anyfunc(n);
+                console.log(timems);
+      if(n == b) {        
+        clearInterval(timerId);
+      }
+      n++;
+  
+    }
+    ,  1000);
+  }
+ 
+  export default printAsyncNumbers;
+  
