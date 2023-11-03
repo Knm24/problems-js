6d4299c8220ab9298cec76335012ac85501c9c48
Knm24 <143102406+Knm24@users.noreply.github.com>
2023-11-03 13:54:16 +0600

    add code pr5 ex8

--- exercise5/problem8/index.ts
+++ exercise5/problem8/index.ts
@@ -1,5 +1,31 @@
-async function fetchCharacterWithMovies() {
-  // Your code
+async function fetchCharacterWithMovies(id: number) {
+
+try {
+
+        const url = `https://swapi.dev/api/people/${id}`;
+
+        const option = {
+            method: "GET"
+        };
+
+        const response = await fetch(url, option);
+
+        if (!response.ok) {
+        throw new Error("throw error response");
+        }
+
+        const data = await response.json();
+
+        return data;
+
+
+        
+    } catch (err) {
+    return Promise.reject(new Error("Promise reject"));
+  }
+
+
 }
+
 
 export default fetchCharacterWithMovies;
