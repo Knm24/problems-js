ea9378e7f79d882c468a5cdbd9b4754949cb6d15
Knm24 <143102406+Knm24@users.noreply.github.com>
2023-11-03 13:38:33 +0600

    add code ex5 pr6 need fix return object

--- exercise5/problem6/index.ts
+++ exercise5/problem6/index.ts
@@ -1,3 +1,27 @@
-async function fetchPokemon() {}
+
+async function fetchPokemon(pokemonName: string) {
+
+try {
+
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+
+    const option = {
+        method: "GET"
+    };
+
+    const response = await fetch(url, option);
+
+    if (!response.ok) {
+      throw new Error("throw error response");
+    }
+
+        const data = await response.json();
+       return data;
+    } catch (err) {
+    return Promise.reject(new Error("Promise reject"));
+  }
+
+
+}
 
 export default fetchPokemon;
