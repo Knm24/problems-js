4885469495eb7446ea311ea135607267933976f4
Knm24 <143102406+Knm24@users.noreply.github.com>
2023-11-03 08:37:02 +0600

    need fix: ex4 pr4

--- exercise5/problem4/index.ts
+++ exercise5/problem4/index.ts
@@ -1,3 +1,41 @@
-class Timer {}
+class Timer {
+    private currTimeMS: number;
+    private startNum: number;
+    private isPaused: boolean;
+    private elapsedTime: number;
+
+    constructor() {
+        this.currTimeMS = new Date().getTime();
+        this.startNum = this.currTimeMS;
+        this.isPaused = false;
+        this.elapsedTime = 0;
+    }
+    
+    start() {
+        while (true) {
+            this.currTimeMS = new Date().getTime();
+            this.elapsedTime = Math.floor((this.currTimeMS - this.startNum) / 1000);
+            console.log(this.elapsedTime);
+
+            if (this.isPaused) {
+                break;
+            }
+        }
+    }
+    
+    pause() {
+        this.isPaused = true;
+    }
+    
+    reset() {
+        this.startNum = new Date().getTime();
+        this.elapsedTime = 0;
+    }
+
+    log() {
+        console.log(this.elapsedTime);
+    }
+}
+
 
 export default Timer;
