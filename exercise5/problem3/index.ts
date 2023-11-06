801282f7f055f97ba87798347d3a206bc4a0136d
Knm24 <sun.forsurface@gmail.com>
2023-11-07 00:46:56 +0600

    fix ex5 pr8

--- exercise5/problem3/index.ts
+++ exercise5/problem3/index.ts
@@ -1,31 +1,30 @@
 function printAsyncNumbers(b: number ) {
 
-    async function anyfunc (n:number) : Promise<number> {
-       
-      let timems = n * 10000;
-      //await console.log('timems:' + timems);
+  async function anyfunc (n:number) : Promise<number> {
+     
+    let timems = n * 10000;
+    //await console.log('timems:' + timems);
 
 
-      await (setTimeout(():void => {}), timems);
+    await (setTimeout(():void => {}), timems);
 
-      await console.log(n);
-      return n;
-    };
+    await console.log(n);
+    return n;
+  };
+
+  let n = 1;
   
-    let n = 1;
-    
-    let timerId = setInterval(async () => {
-        let timems : number;
-        timems = await anyfunc(n);
-                console.log(timems);
-      if(n == b) {        
-        clearInterval(timerId);
-      }
-      n++;
-  
+  let timerId = setInterval(async () => {
+      let timems : number;
+      timems = await anyfunc(n);
+              console.log(timems);
+    if(n == b) {        
+      clearInterval(timerId);
     }
-    ,  1000);
+    n++;
+
   }
- 
-  export default printAsyncNumbers;
-  
+  ,  1000);
+}
+
+export default printAsyncNumbers;
