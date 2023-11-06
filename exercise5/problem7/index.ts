b783cd8898bbdfa751deaa46ebe74f28face3edf
Knm24 <143102406+Knm24@users.noreply.github.com>
2023-11-06 17:01:03 +0600

    add code ex5 pr7

--- exercise5/problem7/index.ts
+++ exercise5/problem7/index.ts
@@ -1,5 +1,54 @@
-async function fetchPokemons() {
-  // Your code
+async function fetchPokemons(names: string[]) {
+
+try {
+
+                const option = {
+                method: "GET"
+                };
+
+
+async function fetchFilms (filmURL : string) {
+       
+       console.log('names::')
+       console.log(   names)
+
+
+        const responseFilms =  await fetch(filmURL, option);
+
+        if (!responseFilms.ok) {
+                throw new Error("throw error response");
+        }
+
+        const getDataFilms =  await responseFilms.json();
+
+         return getDataFilms; 
+
+        
+
 }
+
+
+        const filmPromises = names.map(async (item: string) => {
+
+
+            return await fetchFilms(item); 
+        });
+
+
+           console.log('filmPromises');
+          console.log(filmPromises);
+
+                } catch (err) {
+                return Promise.reject(new Error("Promise reject"));
+                }
+
+}
+
+
+
+//let character = fetchPokemons(["not exist1", "not exist2"]);
+
+//console.log(character);
+
 
 export default fetchPokemons;
