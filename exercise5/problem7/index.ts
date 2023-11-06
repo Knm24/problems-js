3a22a26b4a5b7a38fe71e4a9a6baae0561deac65
Knm24 <143102406+Knm24@users.noreply.github.com>
2023-11-07 01:31:18 +0600

    todo exercise5 - problem7 (4)      × fetch correct result

--- exercise5/problem7/index.ts
+++ exercise5/problem7/index.ts
@@ -1,54 +1,47 @@
 async function fetchPokemons(names: string[]) {
 
-try {
+        try {
+        
+        async function fetchFilms (filmURL : string) {
+               
+                const responseFilms =  await fetch(filmURL);
+        
+                if (!responseFilms.ok) {
+                        return Promise.reject(new Error("smth went wrong"));
 
-                const option = {
-                method: "GET"
-                };
-
-
-async function fetchFilms (filmURL : string) {
-       
-       console.log('names::')
-       console.log(   names)
-
-
-        const responseFilms =  await fetch(filmURL, option);
-
-        if (!responseFilms.ok) {
-                throw new Error("throw error response");
+                }        
+                const pokemon =  await responseFilms.json();
+        
+                return {
+                        id: pokemon.id,
+                        name: pokemon.name,
+                        height: pokemon.height,
+                        weight: pokemon.weight,
+                        image: pokemon.sprites.front_default,
+                      };        
         }
+        
+        
+                const filmPromises = names.map(async (item_name: string) => {       
+        
+                    return await fetchFilms(`https://pokeapi.co/api/v2/pokemon/${item_name}`); 
+                });
+        
+                const filmsDataAll = await Promise.all(filmPromises);
 
-        const getDataFilms =  await responseFilms.json();
 
-         return getDataFilms; 
+                   return { filmsDataAll };
 
         
+                        } catch (err) {
+                                return Promise.reject(new Error("smth went wrong"));
+                        }
+        
+        }
+        
+        
+        
 
-}
-
-
-        const filmPromises = names.map(async (item: string) => {
-
-
-            return await fetchFilms(item); 
-        });
-
-
-           console.log('filmPromises');
-          console.log(filmPromises);
-
-                } catch (err) {
-                return Promise.reject(new Error("Promise reject"));
-                }
-
-}
-
-
-
-//let character = fetchPokemons(["not exist1", "not exist2"]);
-
-//console.log(character);
-
-
-export default fetchPokemons;
+        
+        
+        export default fetchPokemons;
